4ed58263e9ebfe6d8f2d9a0dd4cbb49a7aaa40ad
Lakerolmaker <jacob.n.olsson@hotmail.com>
2018-03-02 08:16:27 +0100

    construction of the city

--- src/main.java
+++ src/main.java
@@ -38,9 +38,44 @@ public class main {
 			intArray.add(tempOfTemps);
 		}
 		
+		int rows = 0;
+		int collums = 0;
+		int vehicles = 0;
+		int rides = 0;
+		int bonus = 0;
+		int steps = 0;
 		
-		System.out.println("done");
 
+		int constuctionIndex = 1;
+		Integer[] setup = intArray.get(0);
+		for(int i = 0; i < setup.length; i++) {
+			
+			if(setup[i] != null) {
+				
+				if(constuctionIndex == 1) {
+					rows = setup[i];
+				}else if(constuctionIndex == 2) {
+					collums = setup[i];
+				}else if(constuctionIndex == 3) {
+					vehicles = setup[i];
+				}else if(constuctionIndex == 4) {
+					rides = setup[i];
+				}else if(constuctionIndex == 5) {
+					bonus = setup[i];
+				}else if(constuctionIndex == 6) {
+					steps = setup[i];
+				}
+				
+				constuctionIndex++;
+			}
+				
+		}
+		
+		
+		City city =  new City(rows , collums , vehicles);
+		
+		System.out.println("dones");
+		
 	}
 
 }
